1e52b3b201d34eca82a456899d26527135d93ace
mayurpatil96k <mayur1792001@gmail.com>
2024-04-23 16:36:05 +0530

    [Mayur]Refactor: Added some more info regarding arrays in file

--- arrays.ts
+++ arrays.ts
@@ -1,4 +1,5 @@
-const names: string[] = [];
+const names:  string[] = [];
 names.push("Dylan"); 
+// if we use readonly then we can not modify original array
 // names.push(50); this line gives error as we declared above as string
 console.log(names)
