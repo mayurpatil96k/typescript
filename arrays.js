cd784db3889fee3f576a560a3cb258b7acfcde7f
mayurpatil96k <mayur1792001@gmail.com>
2024-04-23 16:19:27 +0530

    [Mayur]Add: creating new typescript practice repo

---
+++ arrays.js
@@ -0,0 +1,4 @@
+var names = [];
+names.push("Dylan");
+names.push(50);
+console.log(names);
---
+++ arrays.ts
@@ -0,0 +1,4 @@
+const names: string[] = [];
+names.push("Dylan"); 
+// names.push(50); this line gives error as we declared above as string
+console.log(names)
