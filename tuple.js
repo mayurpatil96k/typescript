50731d90364211c12b79702dd618231a67d5c5aa
mayurpatil96k <mayur1792001@gmail.com>
2024-04-24 12:17:21 +0530

    [Mayur]Add: Adding new tuple program

---
+++ tuple.js
@@ -0,0 +1,7 @@
+var firsttuple;
+firsttuple = [1, "mayur"];
+var sec = ["Mayur", "Arun", "Patil"];
+var a = sec[0], b = sec[1], c = sec[2];
+console.log(a);
+console.log(b);
+console.log(c);
---
+++ tuple.ts
@@ -0,0 +1,10 @@
+let firsttuple: [number,string];
+firsttuple = [1,"mayur"];
+
+
+let sec: readonly[String,String,String] = ["Mayur","Arun","Patil"];
+
+const [a,b,c]=sec;
+console.log(a)
+console.log(b)
+console.log(c)
